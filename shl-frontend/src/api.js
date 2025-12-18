84fe0157450d477870a3c76bd0d8842ce6c39187
Ayush Gupta <95233360+ayu-g22@users.noreply.github.com>
2025-12-18 14:19:08 +0530

    Initial commit

--- shl-frontend/src/api.js
+++ shl-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "http://127.0.0.1:8000" || process.env.VITE_API_BASE;
 
 export const recommend = async (payload) => {
   const res = await axios.post(`${API_BASE}/recommend`, payload);
